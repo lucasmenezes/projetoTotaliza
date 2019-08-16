1b9f5c0ebc44494696243a407734720384b2dbda
Lucas Menezes <lucasmenezes00@gmail.com>
2019-08-16 16:42:30 -0300

    criação da pasta de modelos

---
+++ src/app/modelos/eleicao.js
@@ -0,0 +1,23 @@
+class Eleicao {
+    constructor() {
+        this.id;
+        this.nome;
+        this.descricao;
+        this.tipo;
+        this.data;
+        this.status;
+        this.votosNulos;
+        this.votosBrancos;
+        this.votosAnulados;
+        this.uf;
+        this.quantidadeVagas;
+    }   
+
+    get id() {
+        return this.id;
+    }
+
+
+}
+
+module.exports = Eleicao;
